4feac43c78a267312dac6d734472c879522d812c
Noah R. Johnson <noah.johnson13@ncf.edu>
2018-05-06 21:26:21 -0400

    need pytorch_classification utils

--- Game.py
+++ Game.py
@@ -4,9 +4,7 @@ class Game():
     this class and implement the functions below. This works when the game is
     two-player, adversarial and turn-based.
 
-    Use 1 for player1 and -1 for player2.
-
-    See othello/OthelloGame.py for an example implementation.
+    See miniShogi/miniShogiGame.py for an example implementation.
     """
     def __init__(self):
         pass
